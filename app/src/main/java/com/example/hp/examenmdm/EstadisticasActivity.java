17dff2a4d4655deab39917bf67ee2630fef12560
Moisés Dominguez Mendoza <44880474+MoisesDM@users.noreply.github.com>
2019-03-13 21:22:24 -0600

    2 ultimos puntos

--- app/src/main/java/com/example/hp/examenmdm/EstadisticasActivity.java
+++ app/src/main/java/com/example/hp/examenmdm/EstadisticasActivity.java
@@ -1,14 +1,58 @@
 package com.example.hp.examenmdm;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class EstadisticasActivity extends AppCompatActivity {
-TextView b1,b2,b3,b4,m1,m2,m3,m4;
+    TextView b1, b2, b3, b4, m1, m2, m3, m4,m5,b5;
+    Button Regresra;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_estadisticas);
+
+        this.b1 = findViewById(R.id.b1);
+        this.b2 = findViewById(R.id.b2);
+        this.b3 = findViewById(R.id.b3);
+        this.b4 = findViewById(R.id.b4);
+        this.Regresra = findViewById(R.id.Regresar);
+        this.m1 = findViewById(R.id.m1);
+        this.m2 = findViewById(R.id.m2);
+        this.m3 = findViewById(R.id.m3);
+        this.m4 = findViewById(R.id.m4);
+        this.m5= findViewById(R.id.m5);
+        this.b5= findViewById(R.id.b5);
+
+
+        Intent intent = getIntent();
+        this.b1.setText(intent.getExtras().getString("sumaB"));
+        this.m1.setText(intent.getExtras().getString("sumaM"));
+        this.b2.setText(intent.getExtras().getString("restaB"));
+        this.m2.setText(intent.getExtras().getString("restaM"));
+        this.b3.setText(intent.getExtras().getString("multiB"));
+        this.m3.setText(intent.getExtras().getString("multiM"));
+        this.b4.setText(intent.getExtras().getString("divB"));
+        this.m4.setText(intent.getExtras().getString("divM"));
+        this.m5.setText(intent.getExtras().getString("totalm"));
+        this.b5.setText(intent.getExtras().getString("totalb"));
+
+        this.Regresra.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(
+                        getApplicationContext(),
+                        GameActivity.class
+                );
+                startActivity(intent);
+
+            }
+
+        });
     }
 }
--- app/src/main/java/com/example/hp/examenmdm/GameActivity.java
+++ app/src/main/java/com/example/hp/examenmdm/GameActivity.java
@@ -15,33 +15,45 @@ import android.widget.Toast;
 
 import com.example.hp.examenmdm.Entidades.Datos;
 
+import java.text.BreakIterator;
 import java.util.ArrayList;
 
 public class GameActivity extends AppCompatActivity {
     private ArrayList<Datos> mList;
     private Adapteroperaciones mAdapter;
-    Button btnGenrar,btnvalidar,btnestadistica;
-    TextView num;
+    Button btnGenrar, btnvalidar, btnestadistica;
+    private TextView num;
+    TextView su, sm, res, rm, mu, mm, di, dm;
     EditText numu;
     ImageView va;
-    int suma=0,resta=0,multiplicacion=0,divicion=0;
-    int sumam=0,restam=0,multiplicacionm=0,divicionm=0;
-    int rsuma,rresta,rmulti,rdivicion;
+    int suma = 0, resta = 0, multiplicacion = 0, divicion = 0;
+    int sumam = 0, restam = 0, multiplicacionm = 0, divicionm = 0;
+    int rsuma, rresta, rmulti, rdivicion;
     Spinner spinnerCountries;
 
-    @SuppressLint("WrongViewCast")
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
         initList();
-       spinnerCountries= findViewById(R.id.spOperacions);
-        num=findViewById(R.id.numero1);
-        numu=findViewById(R.id.usuarioNum);
-        va=findViewById(R.id.check);
+        spinnerCountries = findViewById(R.id.spOperacions);
+        num = findViewById(R.id.numero1);
+        numu = findViewById(R.id.usuarioNum);
+        va = findViewById(R.id.check);
         btnGenrar = findViewById(R.id.Generarnum);
-        btnestadistica=findViewById(R.id.VerEstadisticas);
-        btnvalidar=findViewById(R.id.ValidarR);
+        btnestadistica = findViewById(R.id.VerEstadisticas);
+        btnvalidar = findViewById(R.id.ValidarR);
+
+        this.su = findViewById(R.id.tbsuma);
+        this.sm = findViewById(R.id.tmsuma);
+        this.res = findViewById(R.id.tbresta);
+        this.rm = findViewById(R.id.tmresta);
+        this.mu = findViewById(R.id.tbMultiplicacon);
+        this.mm = findViewById(R.id.tmMultiplicacon);
+        this.di = findViewById(R.id.tbdivicion);
+        this.dm = findViewById(R.id.tmdivicion);
+
 
         mAdapter = new Adapteroperaciones(this, mList);
         spinnerCountries.setAdapter(mAdapter);
@@ -51,8 +63,8 @@ public class GameActivity extends AppCompatActivity {
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 Datos clickedItem = (Datos) parent.getItemAtPosition(position);
                 String clickedCountryName = clickedItem.getOpereciones();
-Numeros(clickedCountryName);
-                Toast.makeText(GameActivity.this, clickedCountryName + " selected", Toast.LENGTH_SHORT).show();
+                Numeros(clickedCountryName);
+                Toast.makeText(GameActivity.this, clickedCountryName, Toast.LENGTH_SHORT).show();
             }
 
             @Override
@@ -60,17 +72,25 @@ Numeros(clickedCountryName);
 
             }
         });
-
         btnestadistica.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-
+                int b=suma+resta+multiplicacion+divicion;
+                int m=sumam+restam+multiplicacionm+divicionm;
+                Intent intent = new Intent(v.getContext(), EstadisticasActivity.class);
+                intent.putExtra("sumaB", su.getText());
+                intent.putExtra("sumaM", sm.getText());
+                intent.putExtra("restaB", res.getText());
+                intent.putExtra("restaM", rm.getText());
+                intent.putExtra("multiB", mu.getText());
+                intent.putExtra("multiM", mm.getText());
+                intent.putExtra("multiB", di.getText());
+                intent.putExtra("divM", dm.getText());
+                intent.putExtra("totalb",String.valueOf(b));
+                intent.putExtra("totalm",String.valueOf(m));
+                startActivity(intent);
             }
         });
-
-
-
 
 
     }
@@ -83,34 +103,7 @@ Numeros(clickedCountryName);
                     Toast.makeText(GameActivity.this, "opcion invalida", Toast.LENGTH_SHORT).show();
                 }
             });
-        }
-        else if (clickedCountryName.equals("Suma")) {
-                this.btnGenrar.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        int num1, num2, maximo = 9999, min = 1;
-                        num1 = (int) (Math.random() * 100);
-                        num2 = (int) (Math.random() * 100);
-                        String cad;
-                        rsuma=num1+num2;
-                        cad=num1+"+"+num2;
-                        num.setText(cad);
-                    }
-                });
-            btnvalidar.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    int RsultadoU=Integer.parseInt(numu.getText().toString());
-                    if (RsultadoU==rsuma){
-                        va.setImageDrawable(getDrawable(R.drawable.ic_action_bien));
-                    }else {
-                        va.setImageDrawable(getDrawable(R.drawable.ic_action_error));
-                    }
-                }
-            });
-
-        }
-        else if (clickedCountryName.equals("Resta")) {
+        } else if (clickedCountryName.equals("Suma")) {
             this.btnGenrar.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
@@ -118,24 +111,39 @@ Numeros(clickedCountryName);
                     num1 = (int) (Math.random() * 100);
                     num2 = (int) (Math.random() * 100);
                     String cad;
-                    rresta=num1-num2;
-                    cad=num1+"-"+num2;
+                    rsuma = num1 + num2;
+                    cad = num1 + "+" + num2;
                     num.setText(cad);
                 }
             });
             btnvalidar.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    int RsultadoU=Integer.parseInt(numu.getText().toString());
-                    if (RsultadoU==rresta){
-                        va.setImageDrawable(getDrawable(R.drawable.ic_action_bien));
-                    }else {
-                        va.setImageDrawable(getDrawable(R.drawable.ic_action_error));
+                    try {
+                        int RsultadoU = Integer.parseInt(numu.getText().toString());
+                        if (RsultadoU == rsuma) {
+                            va.setImageDrawable(getDrawable(R.drawable.ic_action_bien));
+                            suma += 1;
+                            su.setText(String.valueOf(suma));
+                            num.setText("");
+                            numu.setText("");
+                        } else {
+                            va.setImageDrawable(getDrawable(R.drawable.ic_action_error));
+                            sumam++;
+                            sm.setText(String.valueOf(sumam));
+                            num.setText("");
+                            numu.setText("");
+                        }
+                    } catch (Exception e) {
+                        Toast.makeText(GameActivity.this, "Ya validaste esta respuesta ", Toast.LENGTH_SHORT).show();
+                        num.setText("");
+                        numu.setText("");
                     }
+
                 }
             });
-        }
-       else  if (clickedCountryName.equals("Multiplicacion")) {
+
+        } else if (clickedCountryName.equals("Resta")) {
             this.btnGenrar.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
@@ -143,25 +151,40 @@ Numeros(clickedCountryName);
                     num1 = (int) (Math.random() * 100);
                     num2 = (int) (Math.random() * 100);
                     String cad;
-                    rmulti=num1*num2;
-                    cad=num1+"*"+num2;
+                    rresta = num1 - num2;
+                    cad = num1 + "-" + num2;
                     num.setText(cad);
                 }
             });
             btnvalidar.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    int RsultadoU=Integer.parseInt(numu.getText().toString());
-                    if (RsultadoU==rmulti){
-                        va.setImageDrawable(getDrawable(R.drawable.ic_action_bien));
-                    }else {
-                        va.setImageDrawable(getDrawable(R.drawable.ic_action_error));
+                    try {
+                        int RsultadoU = Integer.parseInt(numu.getText().toString());
+                        if (RsultadoU == rresta) {
+                            va.setImageDrawable(getDrawable(R.drawable.ic_action_bien));
+                            resta++;
+                            res.setText(String.valueOf(resta));
+                            num.setText("");
+                            numu.setText("");
+                        } else {
+                            va.setImageDrawable(getDrawable(R.drawable.ic_action_error));
+                            restam++;
+
+                            rm.setText(String.valueOf(restam));
+
+                            num.setText("");
+                            numu.setText("");
+                        }
+
+                    } catch (Exception e) {
+                        Toast.makeText(GameActivity.this, "Ya validaste esta respuesta ", Toast.LENGTH_SHORT).show();
+                        num.setText("");
+                        numu.setText("");
                     }
                 }
             });
-
-        }
-        else  if (clickedCountryName.equals("Divicion")) {
+        } else if (clickedCountryName.equals("Multiplicacion")) {
             this.btnGenrar.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
@@ -169,25 +192,80 @@ Numeros(clickedCountryName);
                     num1 = (int) (Math.random() * 100);
                     num2 = (int) (Math.random() * 100);
                     String cad;
-                    rdivicion=num1/num2;
-                    cad=num1+"/"+num2;
+                    rmulti = num1 * num2;
+                    cad = num1 + "*" + num2;
                     num.setText(cad);
                 }
             });
             btnvalidar.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    int RsultadoU=Integer.parseInt(numu.getText().toString());
-                    if (RsultadoU==rdivicion){
-                        va.setImageDrawable(getDrawable(R.drawable.ic_action_bien));
-                    }else {
-                        va.setImageDrawable(getDrawable(R.drawable.ic_action_error));
+
+                    try {
+                        int RsultadoU = Integer.parseInt(numu.getText().toString());
+                        if (RsultadoU == rmulti) {
+                            va.setImageDrawable(getDrawable(R.drawable.ic_action_bien));
+                            multiplicacion++;
+                            mu.setText(String.valueOf(multiplicacion));
+                            num.setText("");
+                            numu.setText("");
+                        } else {
+                            va.setImageDrawable(getDrawable(R.drawable.ic_action_error));
+                            multiplicacionm++;
+                            mm.setText(String.valueOf(multiplicacionm));
+                            num.setText("");
+                            numu.setText("");
+                        }
+
+                    } catch (Exception e) {
+                        Toast.makeText(GameActivity.this, "Ya validaste esta respuesta ", Toast.LENGTH_SHORT).show();
+                        num.setText("");
+                        numu.setText("");
                     }
                 }
             });
 
-        }
+        } else if (clickedCountryName.equals("Divicion")) {
+            this.btnGenrar.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
 
+                    int num1, num2, maximo = 9999, min = 1;
+                    num1 = (int) (Math.random() * 100);
+                    num2 = (int) (Math.random() * 100);
+                    String cad;
+                    rdivicion = num1 / num2;
+                    cad = num1 + "/" + num2;
+                    num.setText(cad);
+                }
+            });
+            btnvalidar.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    try {
+                        int RsultadoU = Integer.parseInt(numu.getText().toString());
+                        if (RsultadoU == rdivicion) {
+                            va.setImageDrawable(getDrawable(R.drawable.ic_action_bien));
+                            divicion++;
+
+                            di.setText(String.valueOf(divicion));
+                            num.setText("");
+                            numu.setText("");
+                        } else {
+                            va.setImageDrawable(getDrawable(R.drawable.ic_action_error));
+                            divicionm++;
+                            dm.setText(String.valueOf(divicionm));
+                            num.setText("");
+                            numu.setText("");
+                        }
+                    } catch (Exception e) {
+                        Toast.makeText(GameActivity.this, "Ya validaste esta respuesta ", Toast.LENGTH_SHORT).show();
+                        num.setText("");
+                        numu.setText("");
+                    }
+                }
+            });
+        }
     }
 
     private void initList() {
